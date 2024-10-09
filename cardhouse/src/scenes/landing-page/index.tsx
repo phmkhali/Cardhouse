b9604255ab29e1ac1e939b052d073f1454960629
khali <106910594+phmkhali@users.noreply.github.com>
2024-10-09 16:23:13 +0200

    Styling adjustment header subtext

--- cardhouse/src/scenes/landing-page/index.tsx
+++ cardhouse/src/scenes/landing-page/index.tsx
@@ -12,13 +12,11 @@ const LandingPage = ({ selectedPage, setSelectedPage }: Props) => {
   return (
     <section id="Home" className="h-[100vh] flex flex-col justify-center">
       {/* CONTENT CONTAINER */}
-      <div className="m-6 p-6 rounded-3xl h-auto flex flex-col items-center">
-        {/* COLOR BLOCKS
-        <div className="w-[250px] h-[250px] bg-primary rounded-3xl"></div> */}
+      <div className="rounded-3xl h-auto flex flex-col items-center ">
         {/* TEXT AND BUTTON */}
-        <div className="w-full flex flex-col p-16 justify-center items-center text-center">
-          <h1 className="font-poppins text-4xl font-bold py-3 md:text-6xl">Say Goodbye to Boring <br></br>Flashcards.</h1>
-          <p className="text-lg mt-6 md:mt-0">Spaced-Repetition-System flashcards you actually want to look at ツ</p>
+        <div className="flex flex-col p-16 justify-center items-center text-center md:w-1/2 ">
+          <h1 className="font-poppins text-4xl font-bold py-3 md:text-6xl">Say Goodbye to Boring Flashcards.</h1>
+          <p className="text-lg mt-6 md:mt-2">Spaced-Repetition-System flashcards you actually want to look at ツ</p>
         </div>
         {/* PICTURE BUTTON*/}
           <img src={PlantPot} className="object-contain w-[150px] transition duration-150 hover:scale-110 md:w-[250px]" />

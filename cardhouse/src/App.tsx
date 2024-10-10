75dd1e8d1412899925cf8cb9eecdd70466b1740e
khali <106910594+phmkhali@users.noreply.github.com>
2024-10-10 16:51:53 +0200

    Rebase folders for login and signup

--- cardhouse/src/App.tsx
+++ cardhouse/src/App.tsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 import Navbar from "./scenes/navbar";
 import { SelectedPage } from "./shared/types";
 import LandingPage from "./scenes/landing-page";
-import Login from "./scenes/account";
+import Login from "./scenes/account/login";
+import SignUp from "./scenes/account/signup";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<LandingPage selectedPage={selectedPage} setSelectedPage={setSelectedPage} />} />
           <Route path="/home" element={<LandingPage selectedPage={selectedPage} setSelectedPage={setSelectedPage} />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </div>
     </Router>

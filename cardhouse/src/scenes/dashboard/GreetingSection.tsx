fb8db0163397ee7df90e23c253430498e94965de
khali <106910594+phmkhali@users.noreply.github.com>
2024-10-17 13:26:38 +0200

    Dashboard button layout

--- cardhouse/src/scenes/dashboard/GreetingSection.tsx
+++ cardhouse/src/scenes/dashboard/GreetingSection.tsx
@@ -6,25 +6,37 @@ type GreetingSectionProps = {
   onAddDeckClick: () => void;
 };
 
-const GreetingSection: React.FC<GreetingSectionProps> = ({ displayName, onAddDeckClick }) => {
+const GreetingSection: React.FC<GreetingSectionProps> = ({
+  displayName,
+  onAddDeckClick,
+}) => {
   return (
-    <div className="w-[90%] h-auto flex flex-col md:flex-row md:items-center justify-start">
+    <div className="w-[90%] h-auto flex flex-col md:flex-row md:items-center md:justify-between">
       {/* GREETING TEXT */}
-      <div className="basis-2/5 h-auto flex flex-col overflow-hidden gap-2">
-        <h1 className="text-4xl font-bold text-secondary">Hi {displayName ? displayName : "Guest"}!</h1>
+      <div className="h-auto flex flex-col overflow-hidden gap-2 ">
+        <h1 className="text-4xl font-bold text-secondary">
+          Hi {displayName ? displayName : "Guest"}!
+        </h1>
         <p className="text-lg">
           Happy to see you here! Let's have a productive study session ^^
         </p>
-        <button
-          onClick={onAddDeckClick}
-          className="w-[200px] h-[50px] bg-primary text-white font-bold rounded-full py-2 px-4"
-        >
-          Add data
-        </button>
+      </div>
+      {/* ADD BUTTON DIV */}
+      <div className="flex justify-start mt-6">
+      <button
+        onClick={onAddDeckClick}
+        className="w-[200px] h-[50px] bg-flower-pink text-white font-bold rounded-full py-2 px-4"
+      >
+        Create deck
+      </button>
       </div>
       {/* PLANT DIV */}
-      <div>
-        <img className="object-contain h-[130px]" src={PlantGif} alt="Animated Plants" />
+      <div className="flex w-auto justify-end ">
+        <img
+          className="object-contain h-[150px]"
+          src={PlantGif}
+          alt="Animated Plants"
+        />
       </div>
     </div>
   );

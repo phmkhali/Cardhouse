bc13f12b8158bc817bedc57810001f98d9cbafbe
khali <106910594+phmkhali@users.noreply.github.com>
2024-10-18 21:38:58 +0200

    Increase tile hover scale

--- cardhouse/src/scenes/dashboard/DeckTile.tsx
+++ cardhouse/src/scenes/dashboard/DeckTile.tsx
@@ -12,7 +12,7 @@ const DeckTile = ({ name, deckId, lastViewed, color, onClick }: Props) => {
   return (
     <div
       onClick={() => onClick(deckId)}
-      className={`mb-4 mr-4 flex items-center justify-center md:text-xl text-sm font-dynapuff h-[80px] w-[100px] md:h-[150px] md:w-[250px] ${color} overflow-hidden rounded-2xl shadow-md shadow-gray-500 text-center hover:scale-95`}
+      className={`mb-4 mr-4 flex items-center justify-center md:text-xl text-sm font-dynapuff h-[80px] w-[100px] md:h-[150px] md:w-[250px] ${color} overflow-hidden rounded-2xl shadow-md shadow-gray-500 text-center hover:scale-110`}
     >
       {name}
     </div>
